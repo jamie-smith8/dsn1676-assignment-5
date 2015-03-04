0acd5639670c2875d46b2f3ca50bcdce1e7fc903
Jamie <smit1365@algonquinlive.com>
2015-03-04 11:29:07 -0500

    very confused

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var $form = $('.form');
+var $todo = $('.todo');
+var $list = $('.list');
+
+$form.on'submit', function (e) {
+    e.preventDefault();
+    
+    var $li = $('<li>');
+    var $todoH2 = $('<h2>').html($todo.val());
+    
+}
