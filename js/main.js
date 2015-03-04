d5cbfbf5ad6f85dc4cb2534e6df3dd3cbbbbac86
Jamie <smit1365@algonquinlive.com>
2015-03-04 12:18:23 -0500

    confused and
    unsure why e.preventDefault() isn't working

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ var $form = $('.form');
 var $todo = $('.todo');
 var $list = $('.list');
 
-$form.on'submit', function (e) {
+$form.on('submit', function (e) {
     e.preventDefault();
     
     var $li = $('<li>');
     var $todoH2 = $('<h2>').html($todo.val());
     
-}
+});
